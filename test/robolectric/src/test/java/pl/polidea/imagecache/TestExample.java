3a15933c637f46bcd9b37f38af4f34172d4d180c
Wojciech Piwonski <abcmax@gmail.com>
2012-10-18 14:44:54 +0200

    - robolectric project in progress

---
+++ test/robolectric/src/test/java/pl/polidea/imagecache/TestExample.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package pl.polidea.imagecache;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+/**
+ * @author Wojciech Piwonski
+ * 
+ */
+@RunWith(RobolectricTestRunner.class)
+public class TestExample {
+
+    @Test
+    public void testExample() {
+        Assert.assertTrue(true);
+    }
+
+}
