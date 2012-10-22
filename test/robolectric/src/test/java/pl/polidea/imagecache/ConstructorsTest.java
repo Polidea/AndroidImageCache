9da4c58bfcbf84d00ddb2ac4a01042940a9a0695
wojciech piwonski <wojciech.piwonski@polidea.pl>
2012-10-22 12:14:57 +0200

    - constructors tests done

--- test/robolectric/src/test/java/pl/polidea/imagecache/ConstructorsTest.java
+++ test/robolectric/src/test/java/pl/polidea/imagecache/ConstructorsTest.java
@@ -3,10 +3,15 @@
  */
 package pl.polidea.imagecache;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Bitmap.CompressFormat;
 
@@ -18,6 +23,12 @@ import com.xtremelabs.robolectric.Robolectric;
  */
 @RunWith(ImageCacheTestRunner.class)
 public class ConstructorsTest {
+
+    private static final int MB = 1024 * 1024;
+
+    private static final int DEFAULT_WORKERS_NUMBER = 1;
+    private static final CompressFormat DEFAULT_COMPRESS_FORMAT = CompressFormat.JPEG;
+    private static final int DEFAULT_COMPRESS_QUALITY = 100;
 
     private Context context;
     private CacheConfig config;
@@ -31,105 +42,270 @@ public class ConstructorsTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void nullConfigFailTest() {
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullConfigContextFailTest() {
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyConfigFailTest() {
+        // given
         config = new CacheConfig();
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test
     public void defaultValuesTest() {
+        // when
         imageCache = new ImageCache(context);
+
+        // then
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
     }
 
     @Test
     public void fullConfigTest() {
-        config = prepareConfig(1, 6 * 1024 * 1024, "/cache", (long) (10 * 1024 * 1024), CompressFormat.JPEG, 80);
+        // given
+        final int workersNumber = 1;
+        final int memoryCacheSize = 6 * 1024 * 1024;
+        final String diskCachePath = File.separator + "cache";
+        final long diskCacheSize = 10 * 1024 * 1024;
+        final CompressFormat compressFormat = CompressFormat.JPEG;
+        final int compressQuality = 80;
+        config = prepareConfig(workersNumber, memoryCacheSize, diskCachePath, diskCacheSize, compressFormat,
+                compressQuality);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        assertEquals(workersNumber, imageCache.getWorkersNumber());
+        assertEquals(memoryCacheSize, imageCache.getMemoryCacheMaxSize());
+        assertEquals(diskCacheSize, imageCache.getDiskCacheMaxSize());
+        assertEquals(diskCachePath, imageCache.getDiskCachePath());
+        assertEquals(compressFormat, imageCache.getCompressFormat());
+        assertEquals(compressQuality, imageCache.getCompressQuality());
     }
 
     @Test
     public void workersNumberDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setWorkersNumber(2);
+        final int workersNumber = 2;
+        config.setWorkersNumber(workersNumber);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(workersNumber, imageCache.getWorkersNumber());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
+
     }
 
     @Test
     public void memorySizeDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setMemoryCacheSize(6 * 1024 * 1024);
+        final int memoryCacheSize = 6 * 1024 * 1024;
+        config.setMemoryCacheSize(memoryCacheSize);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(memoryCacheSize, imageCache.getMemoryCacheMaxSize());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
     }
 
     @Test
     public void pathDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setDiskCachePath("/cache");
+        final String diskCachePath = File.separator + "cache";
+        config.setDiskCachePath(diskCachePath);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(diskCachePath, imageCache.getDiskCachePath());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
     }
 
     @Test
     public void diskSizeDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setDiskCacheSize((long) 10 * 1024 * 1024);
+        final long diskCacheSize = (long) 10 * 1024 * 1024;
+        config.setDiskCacheSize(diskCacheSize);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(diskCacheSize, imageCache.getDiskCacheMaxSize());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
     }
 
     public void compressFormatDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setCompressFormat(CompressFormat.PNG);
+        final CompressFormat compressFormat = CompressFormat.PNG;
+        config.setCompressFormat(compressFormat);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(compressFormat, imageCache.getCompressFormat());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_QUALITY, imageCache.getCompressQuality());
     }
 
     public void compressQualityDefaultLoadingTest() {
+        // given
         config = new CacheConfig();
-        config.setCompressQuality(75);
+        final int compressQuality = 75;
+        config.setCompressQuality(compressQuality);
+
+        // when
         imageCache = new ImageCache(context, config);
+
+        // then
+        assertEquals(compressQuality, imageCache.getCompressQuality());
+
+        final int memoryClass = getMemoryClass();
+        assertEquals(DEFAULT_WORKERS_NUMBER, imageCache.getWorkersNumber());
+        assertEquals(getDefaultMemorySize(memoryClass), imageCache.getMemoryCacheMaxSize());
+        assertEquals(getDefaultDiskSize(memoryClass), imageCache.getDiskCacheMaxSize());
+        assertEquals(getDefaultCachePath(), imageCache.getDiskCachePath());
+        assertEquals(DEFAULT_COMPRESS_FORMAT, imageCache.getCompressFormat());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void workersNumberFailTest() {
-        config = prepareConfig(null, 6 * 1024 * 1024, "/cache", (long) (10 * 1024 * 1024), CompressFormat.JPEG, 80);
+        // given
+        config = prepareConfig(null, 6 * 1024 * 1024, File.separator + "cache", (long) (10 * 1024 * 1024),
+                CompressFormat.JPEG, 80);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void memorySizeFailTest() {
-        config = prepareConfig(1, null, "/cache", (long) (10 * 1024 * 1024), CompressFormat.JPEG, 80);
+        // given
+        config = prepareConfig(1, null, File.separator + "cache", (long) (10 * 1024 * 1024), CompressFormat.JPEG, 80);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void pathFailTest() {
+        // given
         config = prepareConfig(1, 6 * 1024 * 1024, null, (long) (10 * 1024 * 1024), CompressFormat.JPEG, 80);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void diskSizeFileTest() {
-        config = prepareConfig(1, 6 * 1024 * 1024, "/cache", null, CompressFormat.JPEG, 80);
+        // given
+        config = prepareConfig(1, 6 * 1024 * 1024, File.separator + "cache", null, CompressFormat.JPEG, 80);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void compressFormatFailTest() {
-        config = prepareConfig(1, 6 * 1024 * 1024, "/cache", (long) (10 * 1024 * 1024), null, 80);
+        // given
+        config = prepareConfig(1, 6 * 1024 * 1024, File.separator + "cache", (long) (10 * 1024 * 1024), null, 80);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void compressQualityFailTest() {
-        config = prepareConfig(1, 6 * 1024 * 1024, "/cache", (long) (10 * 1024 * 1024), CompressFormat.JPEG, null);
+        // given
+        config = prepareConfig(1, 6 * 1024 * 1024, File.separator + "cache", (long) (10 * 1024 * 1024),
+                CompressFormat.JPEG, null);
+
+        // when
         imageCache = new ImageCache(config);
+
+        // then
+        // see annotation param
     }
 
     private CacheConfig prepareConfig(final Integer workersNumber, final Integer memoryCacheSize,
@@ -155,6 +331,23 @@ public class ConstructorsTest {
             config.setCompressQuality(compressQuality);
         }
         return config;
+    }
+
+    private int getMemoryClass() {
+        final ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        return activityManager.getMemoryClass();
+    }
+
+    private String getDefaultCachePath() {
+        return context.getCacheDir().getPath() + File.separator + "bitmaps";
+    }
+
+    private int getDefaultDiskSize(final int memoryClass) {
+        return memoryClass * MB / 4;
+    }
+
+    private int getDefaultMemorySize(final int memoryClass) {
+        return memoryClass * MB / 8;
     }
 
 }
