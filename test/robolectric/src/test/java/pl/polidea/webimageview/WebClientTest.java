9151c7e670a897525f26b1e9b4098ed1af90b31a
Przemek Jakubczyk <przemyslaw.jakubczyk@polidea.pl>
2012-10-25 13:43:35 +0200

    Added test for WebClient

---
+++ test/robolectric/src/test/java/pl/polidea/webimageview/WebClientTest.java
@@ -0,0 +1,53 @@
+package pl.polidea.webimageview;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class WebClientTest {
+
+    @Test
+    public void testMockingWebCall() {
+        fail();
+    }
+
+    @Test
+    public void testAddingLinkToDownload() {
+        fail();
+    }
+
+    @Test
+    public void testAddingLinkToDownloadWithoudCallback() {
+        fail();
+    }
+
+    @Test
+    public void testAddingTwoSameLinks() {
+        fail();
+    }
+
+    @Test
+    public void testSuccessfulDownload() {
+        fail();
+    }
+
+    @Test
+    public void testFailureDownload() {
+        fail();
+    }
+
+    @Test
+    public void testSuccessfullDownloadingSameLinks() {
+        fail();
+    }
+
+    @Test
+    public void testFailureDownloadingSameLinks() {
+        fail();
+    }
+
+}
