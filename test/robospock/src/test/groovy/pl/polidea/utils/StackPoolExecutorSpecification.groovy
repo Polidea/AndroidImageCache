2ac34e925b988009ceacaa813cece6d5d80763e3
Przemysław Jakubczyk <przemyslaw.jakubczyk@polidea.pl>
2013-02-26 18:23:15 +0100

    Refactoring of unit Test

--- test/robospock/src/test/groovy/pl/polidea/utils/StackPoolExecutorSpecification.groovy
+++ test/robospock/src/test/groovy/pl/polidea/utils/StackPoolExecutorSpecification.groovy
@@ -1,78 +1,54 @@
 package pl.polidea.utils
 
 import spock.lang.Specification
+import spock.lang.Timeout
 
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
 
 class StackPoolExecutorSpecification extends Specification {
 
-    Runnable runnable1 = Mock(Runnable)
-    Runnable runnable2 = Mock(Runnable)
-    Runnable runnable3 = Mock(Runnable)
-    Runnable runnable4 = Mock(Runnable)
-    Runnable runnable5 = Mock(Runnable)
-    Runnable runnable6 = Mock(Runnable)
-    Runnable runnable7 = Mock(Runnable)
-
+    @Timeout(1)
     def "should run 1 task while pending tasks are limited to 3 element list"() {
-        given:
-        StackPoolExecutor executor = new StackPoolExecutor(1, 3)
-        def initLatch = new CountDownLatch(1)
-        def processingDoneLatch = new CountDownLatch(3)
-        def holdingAllRunnables = new CountDownLatch(1)
-        and: "submit long executive task"
-        executor.submit(new LatchRunnable(initLatch))
-        and: "submit other tasks"
-        executor.submit(runnable2)
-        executor.submit(runnable3)
-        executor.submit(runnable4)
-        executor.submit(new WaitingRunnable(processingDoneLatch, holdingAllRunnables))
-        executor.submit(new WaitingRunnable(processingDoneLatch, holdingAllRunnables))
-        executor.submit(new WaitingRunnable(processingDoneLatch, holdingAllRunnables))
+        given: 'awaiting task'
+        def initLock = new ReentrantLock()
+        initLock.lock()
 
-        when:
-        holdingAllRunnables.countDown()
-        initLatch.countDown()
+        def waitingTask = { initLock.newCondition().await() } as Runnable
+
+        and: 'simple runnables'
+        def processingDoneLatch = new CountDownLatch(3)
+        def doneRunnables = [] as Vector
+
+        def runnables = (1..6).collect {
+            new Runnable() {
+                void run() {
+                    processingDoneLatch.countDown()
+                    doneRunnables.add(this)
+                }
+
+                String toString() { "Task $it" }
+            }
+        }
+
+        and: 'tested executable'
+        StackPoolExecutor executor = new StackPoolExecutor(1, 3)
+
+        and: "submit locked task"
+        executor.submit(waitingTask)
+
+        and: "submit rest"
+        runnables.each { executor.submit(it) }
+
+        when: 'release waiting task'
+        initLock.unlock()
+
+        and: 'wait for the rest to finish'
         processingDoneLatch.await()
-        executor.awaitTermination(1L, TimeUnit.SECONDS)
 
         then:
-        0 * runnable2.run()
-        0 * runnable3.run()
-        0 * runnable4.run()
         executor.getQueue().size() == 0
+        doneRunnables == runnables[5..3]
     }
 
-    class LatchRunnable implements Runnable {
-
-        CountDownLatch latch
-
-        LatchRunnable(CountDownLatch latch) {
-            this.latch = latch
-        }
-
-        @Override
-        void run() {
-            latch.await();
-        }
-    }
-
-
-    class WaitingRunnable implements Runnable {
-
-        CountDownLatch signalProcessingDone
-        CountDownLatch waitFor
-
-        WaitingRunnable(CountDownLatch signalProcessingDone, CountDownLatch waitFor) {
-            this.signalProcessingDone = signalProcessingDone
-            this.waitFor = waitFor;
-        }
-
-        @Override
-        void run() {
-            waitFor.await();
-            signalProcessingDone.countDown();
-        }
-    }
 }
